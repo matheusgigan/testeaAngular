5516f6790ed38bba400871e6a6fdd9db60bd150a
KaremLeal <leal.karem@gmail.com>
2025-05-25 17:29:58 -0300

    Estilização Criação de Rotinas

--- frontend/src/app/pages/criar-rotinas/criar-rotinas.component.ts
+++ frontend/src/app/pages/criar-rotinas/criar-rotinas.component.ts
@@ -18,22 +18,26 @@ export class CriarRotinasComponent {
 
   constructor(private router: Router) {}
 
- onSubmit() {
-  const novaRotina = {
-    nome: this.nomeRotina,
-    meta: this.metaRotina,
-    tempo: this.tempoRotina,
-    tipo: this.tipoRotina,
-    diasConcluidos: 0
-  };
+  onSubmit() {
+    const novaRotina = {
+      nome: this.nomeRotina,
+      meta: this.metaRotina,
+      tempo: this.tempoRotina,
+      tipo: this.tipoRotina,
+      diasConcluidos: 0
+    };
 
-  const usuario = JSON.parse(localStorage.getItem('usuarioLogado') || 'null');
-  if (usuario) {
-    const chave = `rotinas_${usuario.email}`;
-    let rotinas = JSON.parse(localStorage.getItem(chave) || '[]');
-    rotinas.push(novaRotina);
-    localStorage.setItem(chave, JSON.stringify(rotinas));
-    this.router.navigate(['/minhasRotinas']);
+    const usuario = JSON.parse(localStorage.getItem('usuarioLogado') || 'null');
+    if (usuario) {
+      const chave = `rotinas_${usuario.email}`;
+      let rotinas = JSON.parse(localStorage.getItem(chave) || '[]');
+      rotinas.push(novaRotina);
+      localStorage.setItem(chave, JSON.stringify(rotinas));
+      this.router.navigate(['/telaInicial']);
+    }
   }
-}
+
+  voltarParaInicio() {
+    this.router.navigate(['/telaInicial']);
+  }
 }
